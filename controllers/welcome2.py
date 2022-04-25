40be9d870d8df265349285917149b24cadca653d
Joel-Garcia-Martinez <1719110587@utectulancingo.edu.mx>
2022-04-25 00:43:09 +0000

    commit on main

--- controllers/welcome2.py
+++
@@ -1,5 +0,0 @@
-import web #importa la libreria web.py
-import pyrebase #importa la libreria para el uso de firebase
-import firebase_config as token #configura el reconocimiento de token o id en firebase
-import json #importa el archivo json 
-
